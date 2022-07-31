dda9af78c68b1199f639149162e1f6c7acc561a1
killshotxd <hassanansari211@gmail.com>
2022-07-31 11:48:12 +0530

    Home Page with animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "../pages/Home.css";
-
+import Zoom from "react-reveal/Zoom";
 import { Link } from "react-router-dom";
+import Slide from "react-reveal/Slide";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to Quizzer</h1>
-      <h2>Take Quizzes in our Quizzer App and level up your skills</h2>
-      <Link to="/QuizSelectionPage">Take Quiz</Link>
+    <div className="center__div">
+      <div className="text__center">
+        <Slide left>
+          <h1>Welcome to Quizzer</h1>
+        </Slide>
+        <Slide right>
+          <h2>Take Quizzes in our Quizzer App and level up your skills</h2>
+        </Slide>
+        <Zoom>
+          <Link to="/QuizSelectionPage" className="btn-68">
+            Take Quiz
+          </Link>
+        </Zoom>
+      </div>
     </div>
   );
 };
