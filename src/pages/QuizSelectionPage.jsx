afe89d03f41518712abd50e9ca74be982e8f93a7
killshotxd <hassanansari211@gmail.com>
2022-08-01 10:12:20 +0530

    fixed padding homepage

--- src/pages/QuizSelectionPage.jsx
+++ src/pages/QuizSelectionPage.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./QuizSelectionPage.css";
-import Shake from "react-reveal/Shake";
+import HeadShake from "react-reveal/HeadShake";
+import Fade from "react-reveal/Fade";
 import Flash from "react-reveal/Flash";
+import { Zoom } from "react-reveal";
 
 const QuizSelectionPage = () => {
   return (
     <div>
       <div className="center-div">
-        <Shake>
+        <HeadShake>
           <h1>
             Choose your desired&nbsp;<span>Quiz</span>&nbsp;Set
           </h1>
-        </Shake>
-
+        </HeadShake>
         <div className="btnDIV">
           <Flash>
             <Link to="/QuizHTML" className="btn-68">
               Take HTML Quiz
             </Link>
+          </Flash>
+          <Fade left>
             <Link to="/QuizCSS" className="btn-68">
               Take CSS Quiz
             </Link>
+          </Fade>
+          <Fade right>
             <Link to="/QuizJS" className="btn-68">
               Take JavaScript Quiz
             </Link>
-          </Flash>
+          </Fade>
         </div>
       </div>
     </div>
