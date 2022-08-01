95cb0f2439616f69552d923155bc4e02a391c06c
killshotxd <hassanansari211@gmail.com>
2022-08-01 09:57:08 +0530

    update quizselectionpage

--- src/pages/QuizSelectionPage.jsx
+++ src/pages/QuizSelectionPage.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./QuizSelectionPage.css";
+import Shake from "react-reveal/Shake";
+import Flash from "react-reveal/Flash";
 
 const QuizSelectionPage = () => {
   return (
     <div>
       <div className="center-div">
-        <h1>
-          Choose your desired&nbsp;<span>Quiz</span>&nbsp;Set
-        </h1>
+        <Shake>
+          <h1>
+            Choose your desired&nbsp;<span>Quiz</span>&nbsp;Set
+          </h1>
+        </Shake>
 
         <div className="btnDIV">
-          <Link to="/QuizHTML" className="btn-68">
-            Take HTML Quiz
-          </Link>
-          <Link to="/QuizCSS" className="btn-68">
-            Take CSS Quiz
-          </Link>
-          <Link to="/QuizJS" className="btn-68">
-            Take JavaScript Quiz
-          </Link>
+          <Flash>
+            <Link to="/QuizHTML" className="btn-68">
+              Take HTML Quiz
+            </Link>
+            <Link to="/QuizCSS" className="btn-68">
+              Take CSS Quiz
+            </Link>
+            <Link to="/QuizJS" className="btn-68">
+              Take JavaScript Quiz
+            </Link>
+          </Flash>
         </div>
       </div>
     </div>
